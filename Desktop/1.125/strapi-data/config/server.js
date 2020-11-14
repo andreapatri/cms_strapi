c4f6235dc5c2408fa49338cf6aab6ebafff0057b
Andrea Diaz <andrea.diaz@sap.com>
2020-11-13 20:01:24 -0500

    added everything

---
+++ Desktop/1.125/strapi-data/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8e23be5b673c17868a58d78502397cc6'),
+    },
+  },
+});
