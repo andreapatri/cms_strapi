b25f5ad6df5395e8f87d852518cee6991764a72c
Andrea Diaz <andrea.diaz@sap.com>
2020-11-14 23:06:43 -0500

    changes

---
+++ Desktop/1.125/tutorial-strapi/my-project/cms_journal/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '47a0741622d4d54788e08217bb22841e'),
+    },
+  },
+});
---
+++ Desktop/1.125/tutorial-strapi/my-project/cms_journal/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '208ab91d-c8d3-461c-a69b-82a97eb2936a'
+};
